81b7fe79226d216cc6f852273d115736c3aef4a1
akshay0910-asnl <akgunner0910@gmail.com>
2023-02-08 12:49:17 +0530

    Carousel changes

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -1,10 +1,16 @@
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent } from "react";
 import SectionArea from "../SectionArea/SectionArea";
 import SectionTitle from "../SectionTitle/SectionTitle";
 import TestimonialsCard from "../TestimonialsCard/TestimonialsCard";
 import Testimonial from '../../types/testimonial';
-import Carousel from "react-simply-carousel";
+import AliceCarousel from 'react-alice-carousel';
 import './Testimonials.scss';
+
+const responsive = {
+    0: { items: 1 },
+    568: { items: 2 },
+    1024: { items: 3 },
+};
 
 
 interface TestimonialsProps {
@@ -13,14 +19,15 @@ interface TestimonialsProps {
 
 const Testimonials: FunctionComponent<TestimonialsProps> = (props) => {
     const { testimonials } = props;
-    const [activeSlide, setActiveSlide] = useState(0);
+    const items = testimonials.map((testimonial, index) => (<TestimonialsCard key={index} testimonial={testimonial} />))
+    //const [activeSlide, setActiveSlide] = useState(0);
     return (<div className="Testimonials">
         <SectionArea>
             <div className="Testimonials__top">
                 <SectionTitle title="Our Testimonials" helperText="Stop wasting time and money designing and managing a website that doesn’t get results. Happiness guaranteed!" />
             </div>
             <div className="Testimonials__cards">
-                <Carousel
+                {/* <Carousel
                     containerProps={{
                         style: {
                             width: "100%",
@@ -77,8 +84,14 @@ const Testimonials: FunctionComponent<TestimonialsProps> = (props) => {
                     }}
                     itemsToShow={2}
                     speed={400}>
-                    {testimonials.map((testimonial, index) => (<TestimonialsCard key={index} testimonial={testimonial} />))}
-                </Carousel>
+                    
+                </Carousel> */}
+                <AliceCarousel
+                    mouseTracking
+                    items={items}
+                    responsive={responsive}
+                    controlsStrategy="alternate"
+                />
             </div>
         </SectionArea>
     </div>);
